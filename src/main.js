8f226056afc10f4aaa826a2e6a516f0062a32538
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2022-10-16 14:39:41 +0200

    Redes sociales completas
    Imagenes añadidas, links a ellas y cambio estilo de pestañas.

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-	function openCity(evt, cityName) {
+	function newtab(evt, opcion) {
 		// Declare all variables
 		var i, tabcontent, tablinks;
 	  
-		// Get all elements with class="tabcontent" and hide them
+		// oculta todos loselementos
 		tabcontent = document.getElementsByClassName("tabcontent");
 		for (i = 0; i < tabcontent.length; i++) {
 		  tabcontent[i].style.display = "none";
 		}
 	  
-		// Get all elements with class="tablinks" and remove the class "active"
+		// coge todas los botones y los desactiva
 		tablinks = document.getElementsByClassName("tablinks");
 		for (i = 0; i < tablinks.length; i++) {
 		  tablinks[i].className = tablinks[i].className.replace(" active", "");
 		}
 	  
-		// Show the current tab, and add an "active" class to the button that opened the tab
-		document.getElementById(cityName).style.display = "block";
+		// coge el elemento con el id de la opcion querida y activa su contenido
+		document.getElementById(opcion).style.display = "block";
 		evt.currentTarget.className += " active";
 	  }
