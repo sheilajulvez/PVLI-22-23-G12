21acfa91177e9dcc25588772a9f336487ba7075e
CiscoGalvan <fragalva@ucm.es>
2022-10-23 12:43:18 +0200

    cosas del coche

--- src/characters/Car.js
+++ src/characters/Car.js
@@ -1,3 +1,8 @@
+
+function random(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+
 export default class Car extends Phaser.GameObjects.Sprite { //exportamos la clase extendida de Phaser
 
 	constructor(scene, x, y) {
@@ -13,24 +18,56 @@ export default class Car extends Phaser.GameObjects.Sprite { //exportamos la cla
 			}), 
 			frameRate: 10, // imágenes/frames por segundo
 			repeat: -1 //para que sea bucle
+			
 		});
 
 		
 		this.play('idle_BlueCar'); //activa la animavcion
 
 
-		this.setScale(2,2);
+		this.setScale(1,1);
 	}
+	
 	
    
 	preUpdate(t, dt){
 		super.preUpdate(t, dt);
 
-		//movimiento de WENGE
-       
-            this.y+=(4*dt)/100;
-      
-        
+		//movimiento de los coches
+            this.y+=(40*dt)/100;
+
+			if(this.y>860)
+			{
+				this.y=0;
+				console.log(5);
+				var pos=random(0,8);
+				//this.x= random(100, 1200);
+				switch(pos)
+				{
+					case 1:
+						this.x=200;
+						break;
+					case 2:
+						this.x=400;
+						break;	
+					case 3:
+						this.x=600;
+						break;
+					case 4:
+						this.x=800;
+						break;
+					case 5:
+						this.x=1000;
+						break;
+					case 6:
+						this.x=1200;
+						break;
+					case 7:
+						this.x=1400;
+						break;
+							
+				}
+			}       
 	}
 
 }
