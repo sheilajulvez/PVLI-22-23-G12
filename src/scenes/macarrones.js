82a9dea7fee6814bc40d0c216a2cb790fd970b60
julvez <sjulvez@ucm.es>
2022-10-25 16:34:31 +0200

    Vida de Wenge

--- src/scenes/macarrones.js
+++ src/scenes/macarrones.js
@@ -7,6 +7,10 @@ export default class Macarrones extends Phaser.Scene { //creamos la escena expor
 		super({key: 'tomatico'}) 
 	}
 
+	init(){ // inicializar, se ejecuta cada vez que reiniciamos o iniciamos por primera vez
+		this.life=0;
+	}
+
 	preload(){
 		// Cargamos el Spritesheet								//indicamos el alto y alto de todas las imágenes dentro del Spritesheet
 		this.load.image("fondo","assets/Road.png");
@@ -14,7 +18,8 @@ export default class Macarrones extends Phaser.Scene { //creamos la escena expor
 		this.load.spritesheet('Car', 'assets/BlueCar.png', {frameWidth:200 , frameHeight:280});
 		// Cargamos el Atlas (el atlas está compuesto por un archivo de imagen y un JSON que describe lo que contiene)
 		//this.load.atlas('seaAnimals', 'assets/Sea/seacreatures.png', 'assets/Sea/seacreatures.json')
-		
+
+
 	}
 
 	create(){
@@ -25,6 +30,13 @@ export default class Macarrones extends Phaser.Scene { //creamos la escena expor
 		this.add.image(500,350,'fondo');
 		new Wenge(this, 400, 600); //creamos a nuestro personaje, nuestra Wenge
 		new Car(this, 600, 100); //creamos a nuestro personaje, nuestra Car
+		this.lifetext=this.add.text (900,650,'LIFE: 0', {
+			fontsize: '20px',
+			fill:'#fff',
+			fontFamily:'verdana,arial, sans-serif'
+
+
+		});
 		//new SeaAnimals(this, 200, 200);
 	}
 }
