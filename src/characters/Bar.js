6ceb5c22f81f5df29dd5aaa165e2e9adb38cfc81
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2022-11-06 13:51:08 +0100

    barra

---
+++ src/characters/Bar.js
@@ -0,0 +1,38 @@
+export default class Bar  {
+    constructor(scene, x, y) {
+        this.bar = new Phaser.GameObjects.Graphics(scene);
+        this.x =x;
+        this.y =y;
+        this.value=0;
+        this.maximo=40000;
+        this.tamx=110;
+        this.tamy=30;
+        this.p = this.tamx / this.maximo;
+        this.draw();
+        scene.add.existing(this.bar);
+    }
+    aumenta(cant){
+        this.value+=cant;
+        //si el valor supera el máximo entonces lo igualas al máximo
+        if (this.value > this.maximo){
+            this.value= this.maximo;
+        }
+        //dibujas la barra
+        this.draw();
+        //devuelves un bool que indica si el valor es el mismo que el de la barra
+        return (this.value === this.maximo);
+    }
+    draw(){
+        //borra la barra actual
+        this.bar.clear();
+         //  barra del fondo(margen):
+         this.bar.fillStyle(0x000000);
+         this.bar.fillRect(this.x, this.y, this.tamx+4, this.tamy+4);
+         //barra de vida
+         this.bar.fillStyle(0xffffff);
+         this.bar.fillRect(this.x + 2, this.y + 2, this.tamx, this.tamy);
+        this.bar.fillStyle(0xFDEE35);
+        var d = Math.floor(this.p * this.value);
+        this.bar.fillRect(this.x + 2, this.y + 2, d, this.tamy);
+    }
+}
--- src/scenes/generical.js
+++ src/scenes/generical.js
@@ -1,6 +1,6 @@
 import Wenge from '../characters/Wenge.js'; //importamos al caracter de Wenge
 import FondoMove from '../characters/FondoMove.js'; //importamos las distintas casas del fondo
-
+import Barra from '../characters/Bar.js';//importamos la clase bar que indicará por donde va el nivel;
 export default class Generical extends Phaser.Scene { //creamos la escena exportada/extendida de Phaser
 	constructor(nameScene){
 		super({key: nameScene}); 
@@ -12,12 +12,11 @@ export default class Generical extends Phaser.Scene { //creamos la escena export
 	 update(time, delta) {    
 		  let x = Math.random() * this.game.config.width 
 		  let y = 0
+		  console.log(time);
+			this.Barra.aumenta(time/1000);
+			//this.Barra.draw();
 	  }
-
-
-
 	preload(){
-		// Cargamos el Spritesheet					
 		this.load.image('fondo','assets/Road.jpg');
         this.load.spritesheet('Wenge', 'assets/wengeSprite.png', {frameWidth: 633, frameHeight:394});
 		this.load.spritesheet('FondoMove', 'assets/arbol.png', {frameWidth:	128 , frameHeight:	120	});
@@ -25,6 +24,9 @@ export default class Generical extends Phaser.Scene { //creamos la escena export
 	}
 
 	create(){
+		//añadimos los graficos para la barra
+		
+
 		this.add.image(500,350,'fondo');
 			this.lifetext=this.add.text (900,650,'LIFE: 3', {
 			fontsize: '20px',
@@ -42,5 +44,6 @@ export default class Generical extends Phaser.Scene { //creamos la escena export
 			this.Arboles.add (new FondoMove(this, 950,200*i));
 			}
         new Wenge(this, 400, 600); //creamos a nuestro personaje, nuestra Wenge
+		this.Barra = new Barra(this, 10, 10);
 	}
 }
