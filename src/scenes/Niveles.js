a2796c6a658107d2b46fd44734924d94a1ba113e
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2022-11-16 17:16:48 +0100

    posiciones botones

--- src/scenes/Niveles.js
+++ src/scenes/Niveles.js
@@ -22,9 +22,11 @@ export default class Niveles extends Phaser.Scene {
         
         //Nivel[0];
         this.add.image(500,350,'mapaciudad');
-
-        this.Nivel.push(new Button(this, 200,200, 'boton', 'tomatico')) 
-        this.Nivel.push(new Button(this, 200,400, 'boton', 'EscenaHablar')) 
+        this.Nivel.push(new Button(this, 135,485, 'boton', 'tomatico')) 
+        this.Nivel.push(new Button(this, 317,280, 'boton', 'EscenaHablar')) 
+        this.Nivel.push(new Button(this, 720,545, 'boton', 'tomatico')) 
+        this.Nivel.push(new Button(this, 750,140, 'boton', 'EscenaHablar')) 
+        this.Nivel.push(new Button(this, 925,265, 'boton', 'tomatico'))  
         
         // primBot =this.add.image(500,350,'boton').setInteractive();
        // boton =this.add.image(500,350,'boton');
