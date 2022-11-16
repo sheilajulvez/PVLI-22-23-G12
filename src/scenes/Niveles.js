588e5f0252303d8660c1608e9a9d662e79c5e35f
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2022-11-16 16:34:42 +0100

    Cambio de botones, script general para un botón

--- src/scenes/Niveles.js
+++ src/scenes/Niveles.js
@@ -1,14 +1,68 @@
+import Button from '../components/Buttom.js';
 
+//falta expecificar que un nivel desbloquea el siguiente
 export default class Niveles extends Phaser.Scene {
     constructor(){
-        super('MapNiveles');
-         let Nivel =[ ];
-         for (let i=0; i<5; i++) Nivel.push();
+        super({ key: 'MapNiveles'});
+        
+        this.Nivel =[ ];
+           
+         //posicion + nombre de la imagen
+       // this.NivButton= this.relatedScene.add.image(500,200,'next_level');
+      //  this.add.image(500,100, 'boton');
+        // for (let i=0; i<5; i++) Nivel.push(this.NivButton);
     }
     preload(){
         this.load.image("mapaciudad","assets/mapaciudad.png");
+        //guardas la imagen
+        this.load.image('boton',"assets/boton.png");
     }
     create(){
+        
+        
+        //Nivel[0];
         this.add.image(500,350,'mapaciudad');
+
+        this.Nivel.push(new Button(this, 200,200, 'boton', 'tomatico')) 
+        this.Nivel.push(new Button(this, 200,400, 'boton', 'EscenaHablar')) 
+        
+        // primBot =this.add.image(500,350,'boton').setInteractive();
+       // boton =this.add.image(500,350,'boton');
+       // this.image.on('pointerdown',()=>{
+       //     console.log("fg");
+			//this.relatedScene.scene.start('MapNiveles');
+		//});
+        //this.add.image(500,350,'boton');
+        
+
     }
 }
+
+
+
+
+/*
+//crea un var de reiniciarbutton en game
+
+	
+	* Creación de los elementos de la escena principal de juego
+	
+
+		// Para seleccionar botones con teclas, creamos el objeto tecla y un int al que se apunta actualmente
+		var self=this;
+    	let i = 0;
+		var buttons = [];
+		levels.forEach(level => {
+			level.setScene(this, function(){self._keyboard.setBeingUsed(i)});
+			buttons[i] = level.button;
+      		i++;
+		});
+		//console.log(this.buttons);
+		
+		this._keyboard.loadButtonArray(buttons);
+		
+        // .setOrigin(0.5);
+		this.input.keyboard.once('keydown-SPACE', () => {
+            this.scene.start('battleScene');
+        });
+	}*/
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -1,25 +1,21 @@
-import NextLevelButtom from '../components/NextLevelButtom.js';
-import QuitButtom from '../components/QuitButtom.js';
-import ShopButtom from '../components/ShopButtom.js';
 
+import Button from '../components/Buttom.js';
 export default class menu extends Phaser.Scene{
 	constructor(){
 		super({key:'menu'});
-		this.NextLevelButtom=new NextLevelButtom(this);
-		this.QuitButtom=new QuitButtom(this);
-		this.ShopButtom=new ShopButtom(this);
+		this.Botones =[ ];
 	}
 	preload(){
 		this.load.image("fondoMenu","assets/menu_fondo2.jpg");
-		this.NextLevelButtom.preload();
-		this.QuitButtom.preload();
-		this.ShopButtom.preload();
+		this.load.image("next_level","assets/next_level.jpg");
+		this.load.image('quit_buttom',"assets/exit1.jpg");
+		this.load.image('shop_button','assets/shop.jpg');
 
 	}
 	create(){
 		this.add.image(500,350,'fondoMenu');
-		this.NextLevelButtom.create();
-		this.QuitButtom.create();
-		this.ShopButtom.create();
+		this.Botones.push(new Button(this, 400,200, 'next_level', 'MapNiveles')) 
+        this.Botones.push(new Button(this, 400,600, 'quit_buttom', 'inicio')) 
+        this.Botones.push(new Button(this, 400,400, 'shop_button', 'shop')) 
 	}
 }
