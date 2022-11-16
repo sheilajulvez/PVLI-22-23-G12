5027888f18d50bfba99fe718fd8458c6d1bad342
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2022-11-16 16:34:42 +0100

    Cambio de botones, script general para un botón

---
+++ src/components/Buttom.js
@@ -0,0 +1,32 @@
+export default class Button extends Phaser.GameObjects.Sprite{
+
+	constructor(scene,x,y, name, scarga){
+		console.log(name);
+		super(scene, x, y,name);
+		this.scene.add.existing(this);
+		this.scarga = scarga;
+		this.scene = scene;
+        this.setInteractive();
+		this.create();
+	}
+
+	create(){
+		
+		/*this.startButton.on('pointerover',() => {
+			this.startButton.setFrame(1);
+		});
+
+		this.startButton.on('pointerout',() => {
+			this.startButton.setFrame(0);
+		
+		});*/
+
+		this.on('pointerdown',() => {
+
+			this.scene.scene.start(this.scarga);
+		});
+
+	}
+
+
+}
