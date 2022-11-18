2d292d1e5be9a43e79e6b5327f4cdd114b507fe0
julvez <sjulvez@ucm.es>
2022-11-18 18:41:55 +0100

    NIVEL 1 Y 3

--- src/scenes/Niveles/Aceite.js
+++ src/scenes/Niveles/Aceite.js
@@ -2,12 +2,13 @@
 import Car from '../../characters/Car.js';//importamos a los Coches
 import Generical from '../../scenes/generical.js';
 import Van from '../../characters/Van.js';
+import { Moto } from '../../characters/Motos.js';
 import Pool  from '../../characters/Pool.js';
 
 function random(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
-
+var vanSpawn=0;
 export default class Aceite extends Generical { //creamos la escena exportada/extendida de Phaser
 	constructor(){
 
@@ -20,41 +21,57 @@ export default class Aceite extends Generical { //creamos la escena exportada/ex
 		this.Inicia(this);
 		this.load.spritesheet('Car', 'assets/BlueCar.png', {frameWidth:200 , frameHeight:280});
 		this.load.spritesheet('Van', 'assets/WhiteCar.png', {frameWidth:166 , frameHeight:	233	});
+		this.load.spritesheet('Bike','assets/Moto.jpg',{frameWidth:166 , frameHeight:490});
 	}
 
 	create(){
 		super.create();
 		this.timeDelta=0;
-		this.car=new Car(this,0,-1000);
-		this.physics.add.existing(this.car);
-
+		
 		let arrayCoches=[];
 		
+
 		for(let i=0; i<5;i++)
 		{
-			arrayCoches.push(this.car);
+			let car=new Car(this,0,-1000);
+			arrayCoches.push(car);
+			
 		}
 		
+
+		console.log(arrayCoches.length);
+		
 		this.poolCar=new Pool(this,5,arrayCoches);	
-		this.van=new Van(this,0,-1000);
-		this.physics.add.existing(this.van);
+		this.physics.add.collider(this.player, this.poolCar.getPhaserGroup());
+		
+		
 
-
-				
 		let arrayVan=[];
 		for(let i=0; i<5;i++)
 		{
-			arrayVan.push(this.van);
+			let van=new Van(this,0,-1000);
+			arrayVan.push(van);
 		}
 		this.poolVan=new Pool(this,5,arrayVan);
+		this.physics.add.collider(this.player,this.poolVan.getPhaserGroup());
+
+
+
 		
-		//this.physics.add.existing(this.player);// lo haces objeto físico
+		
+		let arrayBike=[];
+		for(let i=0;i<3;i++)
+		{
+			let bike=new Moto(this,0,-1000);
+			arrayBike.push(bike);
+		}
+		this.poolBike=new Pool(this,3,arrayBike);
+		
+		
+		this.physics.add.collider(this.player, this.poolBike.getPhaserGroup());
 
+		
 
-
-		this.physics.add.collider(this.player, this.car);
-		if(this.physics.collide(this.player, this.car)) {
-    		console.log("Hay colisión");}
 		
 		
 	}
@@ -66,22 +83,33 @@ export default class Aceite extends Generical { //creamos la escena exportada/ex
 	{
 		this.poolVan.release(vehicles);
 	}
+	BikeisOut(vehicles)
+	{
+		this.poolBike.release(vehicles);
+	}
+	collision()
+	{
+		if(this.physics.collide(this.player, this.car)||this.physics.collide(this.player, this.van)) {
+    		console.log("Hay colisión");}
+	}
 	update(t,dt)
 	{
 		
 		super.update(t,dt);
+		this.collision();
 		this.timeDelta= this.timeDelta+dt;
 		if(this.Barra.fin()){
-			this.scene.start("EscenaHablar",{name:"Aceite_fin"} )
+			console.log("esto entra?")
+			this.scene.start("EscenaHablar",{name:"tomatico_fin"} )
 		}
-		
-		if(this.timeDelta>4000)
+		if(this.timeDelta>2000)
 		{
 	    var rand=random(0,1);
-	    if (rand===0)
+	    if (rand===0)				//respawm car
 		{
 			
 			    let pos=random(0,5);
+				vanSpawn++;
 				this.timeDelta=0;
 				let vehicleX=0;
 					switch(pos)
@@ -104,15 +132,40 @@ export default class Aceite extends Generical { //creamos la escena exportada/ex
 						   case 5:
 							vehicleX=870;
 							   break;
+							
 					}
-					
+				console.log('pos='+pos)
+				console.log(vanSpawn);
 				this.poolCar.spawn(vehicleX,0,'idle_BlueCar');
 
 			
 		}
-		else if(rand===1)
+		else if(rand===1)			//respawn bike
 		{
 			let pos=random(0,1);
+			vanSpawn++;
+			this.timeDelta=0;
+			let vehicleX=0;
+				switch(pos)
+					{
+						case 0:
+							vehicleX=350;
+							break;
+						case 1:
+							vehicleX=740;
+							break;
+					}
+				console.log(vanSpawn);
+				this.poolBike.spawn(vehicleX,0,'idle_bike');
+		}
+		
+		}
+		
+		
+	    if(vanSpawn===4)
+		{
+			let pos=random(0,1);
+			vanSpawn=0;
 			this.timeDelta=0;
 			let vehicleX=0;
 			switch(pos)
@@ -124,12 +177,8 @@ export default class Aceite extends Generical { //creamos la escena exportada/ex
 						vehicleX=740;
 						break;
 				}
-			console.log("respawn");
+			
 			this.poolVan.spawn(vehicleX,0,'idle_WhiteCar');
-		}
-		}
-
-		
-		
-	}
+	 	}
+	 }
 }
--- src/scenes/Niveles/macarrones.js
+++ src/scenes/Niveles/macarrones.js
@@ -1,7 +1,6 @@
 import Car from '../../characters/Car.js';//importamos a los Coches
 import Generical from '../../scenes/generical.js';
 import Van from '../../characters/Van.js';
-import { Moto } from '../../characters/Motos.js';
 import Pool  from '../../characters/Pool.js';
 
 
@@ -9,7 +8,7 @@ function random(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-var vanSpawn=0;
+
 export default class Macarrones extends Generical { //creamos la escena exportada/extendida de Phaser
 	constructor(){
 
@@ -22,7 +21,7 @@ export default class Macarrones extends Generical { //creamos la escena exportad
 		this.Inicia(this);
 		this.load.spritesheet('Car', 'assets/BlueCar.png', {frameWidth:200 , frameHeight:280});
 		this.load.spritesheet('Van', 'assets/WhiteCar.png', {frameWidth:166 , frameHeight:	233	});
-		this.load.spritesheet('Bike','assets/Moto.jpg',{frameWidth:166 , frameHeight:490});
+		
 	}
 
 	create(){
@@ -56,21 +55,6 @@ export default class Macarrones extends Generical { //creamos la escena exportad
 		this.poolVan=new Pool(this,5,arrayVan);
 		this.physics.add.collider(this.player,this.poolVan.getPhaserGroup());
 
-
-
-		
-		
-		let arrayBike=[];
-		for(let i=0;i<3;i++)
-		{
-			let bike=new Moto(this,0,-1000);
-			arrayBike.push(bike);
-		}
-		this.poolBike=new Pool(this,3,arrayBike);
-		
-		
-		this.physics.add.collider(this.player, this.poolBike.getPhaserGroup());
-
 		
 
 		
@@ -84,10 +68,7 @@ export default class Macarrones extends Generical { //creamos la escena exportad
 	{
 		this.poolVan.release(vehicles);
 	}
-	BikeisOut(vehicles)
-	{
-		this.poolBike.release(vehicles);
-	}
+	
 	collision()
 	{
 		if(this.physics.collide(this.player, this.car)||this.physics.collide(this.player, this.van)) {
@@ -143,28 +124,10 @@ export default class Macarrones extends Generical { //creamos la escena exportad
 		}
 		else if(rand===1)			//respawn bike
 		{
-			let pos=random(0,1);
 			vanSpawn++;
 			this.timeDelta=0;
-			let vehicleX=0;
-				switch(pos)
-					{
-						case 0:
-							vehicleX=350;
-							break;
-						case 1:
-							vehicleX=740;
-							break;
-					}
-				console.log(vanSpawn);
-				this.poolBike.spawn(vehicleX,0,'idle_bike');
-		}
-		
-		}
-		
-		
-	    if(vanSpawn===4)
-		{
+			if(vanSpawn===2)
+			{
 			let pos=random(0,1);
 			vanSpawn=0;
 			this.timeDelta=0;
@@ -180,6 +143,13 @@ export default class Macarrones extends Generical { //creamos la escena exportad
 				}
 			
 			this.poolVan.spawn(vehicleX,0,'idle_WhiteCar');
-	 	}
+	 		}
+			
+		}
+		
+		}
+		
+		
+	    
 	 }
 }
