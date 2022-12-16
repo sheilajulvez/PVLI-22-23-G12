0f8b1a29423fb6bb4eb069be4eea75c50af9b08b
CristinaMora <100274708+CristinaMora@users.noreply.github.com>
2022-12-16 17:14:59 +0100

    Update Niveles.js

--- src/scenes/Niveles.js
+++ src/scenes/Niveles.js
@@ -31,7 +31,7 @@ export default class Niveles extends Phaser.Scene {
         this.Nivel.push(new Button(this, 317,280, 'boton', 'EscenaHablar','Manzanilla', this.stay,2,this.wenge,this.money,this.music)) 
         this.Nivel.push(new Button(this, 720,545, 'boton', 'EscenaHablar','Aceite',this.stay,3,this.wenge,this.money,this.music)) 
         this.Nivel.push(new Button(this, 750,140, 'boton', 'EscenaHablar','Croquetas',this.stay,4,this.wenge,this.money,this.music)) 
-        this.Nivel.push(new Button(this, 925,265, 'boton', 'EscenaHablar','Arsenico',this.stay,5,this.wenge,this.money,this.music))  
+        this.Nivel.push(new Button(this, 925,265, 'boton', 'EscenaHablar','Arsenico',this.stay,1,this.wenge,this.money,this.music))  
         //mostramos el dinero disponible
         this.money.SetScene(this);
         this.money.ShowMoney();
